ef91ad47803348085d8a55f8384eaea6e5732167
ChanteleNkomo <nkomochantele@gmail.com>
2022-12-06 21:17:33 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+/* responsive nav */
+
+function openNav() {
+  let nav = document.getElementById("showmenu");
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
+
+/* end of nav */
